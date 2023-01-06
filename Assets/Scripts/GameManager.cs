313a2c838eb7190874f1b5af507510ec141006f5
Unreal <scott0202@kaist.ac.kr>
2023-01-06 20:41:11 +0900

    add scenes and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+enum Phase
+{
+    Title,
+    GetOrder,
+    Prepare,
+    Topping,
+    Result,
+}
+
+public class GameManager : MonoBehaviour
+{
+    private GameManager instance;
+    public GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    private Phase phase = Phase.Title;
+    private bool prepareSuccess = false;
+    private bool assembleSuccess = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoToGetOrder()
+    {
+        SceneManager.LoadScene("GetOrderPhase");
+        phase = Phase.Prepare;
+    }
+
+    public void GoToPrepare()
+    {
+        SceneManager.LoadScene("PreparePhase");
+        phase = Phase.Prepare;
+    }
+
+    public void GoToTopping()
+    {
+        SceneManager.LoadScene("ToppingPhase");
+        phase = Phase.Topping;
+    }
+
+    public void GoToResult()
+    {
+        SceneManager.LoadScene("ResultPhase");
+        phase = Phase.Result;
+    }
+
+    public void Done()
+    {
+        // 성공/실패 판정
+        if (prepareSuccess && assembleSuccess)
+        {
+            // 성공
+        }
+        else
+        {
+            // 실패
+        }
+    }
+}
